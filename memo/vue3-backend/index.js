d2b62d95d80766524998c55c6ece0096bc457e3d
deasble <hdjs0814@naver.com>
2022-06-17 09:31:19 +0900

    backend 연결시 CORS 해결

--- memo/vue3-backend/index.js
+++ memo/vue3-backend/index.js
@@ -4,8 +4,12 @@ const port = 3000
 
 const memos =["메모 1", "메모 2", "메모 3", "메모 4"];
 
-app.get('/memos', (req, res) => {
+app.get('/api/memos', (req, res) => {
   res.send(memos)
+})
+
+app.post('/api/memos', (req, res) => {
+    res.send(memos)
 })
 
 app.listen(port, () => {
