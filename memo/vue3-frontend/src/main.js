2b0e360cc5b4b08643a945e672003e97c1916de8
deasble <hdjs0814@naver.com>
2022-06-23 16:55:53 +0900

    tailwindcss 설치 및 scss 설치

--- memo/vue3-frontend/src/main.js
+++ memo/vue3-frontend/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
-import App from "./App.vue";
-import store from "./store";
+import App from "@/App.vue";
+import store from "@/store";
+import "@/assets/scss/index.scss";
 
 createApp(App).use(store).mount("#app");
