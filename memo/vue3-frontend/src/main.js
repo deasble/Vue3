1f265367fd32ae2762c1c32b376129c204447730
deasble <hdjs0814@naver.com>
2022-06-23 17:12:36 +0900

    fontawesome 설치

--- memo/vue3-frontend/src/main.js
+++ memo/vue3-frontend/src/main.js
@@ -2,5 +2,10 @@ import { createApp } from "vue";
 import App from "@/App.vue";
 import store from "@/store";
 import "@/assets/scss/index.scss";
+import { icon } from "@/assets/icon/icons.js";
 
-createApp(App).use(store).mount("#app");
+const app = createApp(App);
+
+icon(app);
+
+app.use(store).mount("#app");
