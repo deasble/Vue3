89ac7c510ede332299b1f4270bacd2c50b7ca584
deasble <hdjs0814@naver.com>
2022-06-28 17:30:21 +0900

    CORS 해결

--- login/frontend/vue.config.js
+++ login/frontend/vue.config.js
@@ -9,3 +9,14 @@ module.exports = {
     config.resolve.alias.set("@", path.resolve(__dirname, "src/"));
   },
 };
+
+module.exports = {
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
+    },
+  },
+};
