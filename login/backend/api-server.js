eb53852dd4d431bfadbe6e77c7544d7acc7ac336
deasble <hdjs0814@naver.com>
2022-06-28 17:30:51 +0900

    front back 연결

--- login/backend/api-server.js
+++ login/backend/api-server.js
@@ -2,8 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/api/account', (req, res) => {
+  res.send({
+    mid: 3,
+    name: '홍길동'
+  })
 })
 
 app.listen(port, () => {
--- login/frontend/src/App.vue
+++ login/frontend/src/App.vue
@@ -1,26 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="app">
+    <div class="loginid">
+      <span>아이디</span>
+      <input type="text" />
+    </div>
+    <div class="loginpw">
+      <span>비밀번호</span>
+      <input type="text" />
+    </div>
+    <hr />
+    <button @click="login"><span>로그인</span></button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent } from "vue";
+import axios from "axios";
 
-export default {
-  name: "App",
-  components: {
-    HelloWorld,
+export default defineComponent({
+  components: {},
+  setup() {
+    const login = () => {
+      console.log("로그인");
+    };
+
+    axios.get("/api/account").then((req) => {
+      console.log(req);
+    });
+
+    return {
+      login,
+    };
   },
-};
+});
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
