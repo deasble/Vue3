1461993b3f4e6eaf4ed6fcc7bc0f54cf70fbb3ed
deasble <hdjs0814@naver.com>
2022-07-04 16:31:18 +0900

    memo project 삭제

--- memo/frontend/src/api/index.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const FETCH_MEMO_LIST = () => axios.get("/api/memos");
-
-export const CREATE_MEMO = (payload) => axios.post("/api/memos", payload);
-
-export const UPDATE_MEMO_LIST = (payload) => axios.put('/api/memo', payload)
-
-export const DELETE_MEMO_LIST = (payload) => axios.delete(`/api/memos/${payload}`)
