58d71d12ad71fc6cc364fd44c7f2327838790e56
deasble <hdjs0814@naver.com>
2022-07-11 16:12:00 +0900

    리팩터링

--- memo/frontend/src/components/Modal/ModalHead.vue
+++ memo/frontend/src/components/Modal/ModalHead.vue
@@ -2,10 +2,7 @@
   <header class="modal_head">
     <div class="modal_head_box">
       <span>할 일 수정</span>
-      <button
-        class="btn"
-        @click="$emit('closeModal', { id: null, memo: '', status: '' })"
-      >
+      <button class="btn" @click="$emit('closeModal', reset)">
         <font-awesome-icon icon="fa-solid fa-x" />
       </button>
     </div>
@@ -18,7 +15,15 @@ import { defineComponent } from "vue";
 export default defineComponent({
   emits: ["closeModal"],
   setup() {
-    return {};
+    const reset = {
+      id: null,
+      memo: "",
+      status: "",
+    };
+
+    return {
+      reset,
+    };
   },
 });
 </script>
@@ -26,6 +31,7 @@ export default defineComponent({
 <style lang="scss" scoped>
 .modal_head {
   padding: 20px 10px;
+
   .modal_head_box {
     display: flex;
     justify-content: space-between;
