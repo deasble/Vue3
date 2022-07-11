0a0ed5264e6f56cf8d37db2d2cac993901831368
deasble <hdjs0814@naver.com>
2022-07-11 10:11:20 +0900

    footer

---
+++ memo/frontend/src/components/Todo/TodoFooter.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer class="footer">
+    <p class="text-sm">ⓒ 2022. deasble all rights reserved.</p>
+  </footer>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {},
+});
+</script>
+
+<style lang="scss" scoped>
+.footer {
+  text-align: center;
+  background: #fff;
+  width: 100%;
+  padding: 20px 0;
+}
+</style>
