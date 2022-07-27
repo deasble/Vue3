cb567e96b64e1a311009ddb884c655e12cf40b19
deasble <hdjs0814@naver.com>
2022-07-27 15:52:19 +0900

    memo host 충돌 오류 해결

--- login/frontend/vue.config.js
+++ login/frontend/vue.config.js
@@ -14,7 +14,7 @@ module.exports = {
   devServer: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "http://localhost:3001",
         changeOrigin: true,
       },
     },
